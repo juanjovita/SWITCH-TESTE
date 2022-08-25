380657b945c92fa3377a5d336b2bf5377bed904f
juanjovita <juanjovita22@gmail.com>
2022-08-25 17:34:17 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-z47k8i
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-z47k8i)
---
+++ script.js
@@ -0,0 +1,24 @@
+console.log('Hello!');
+
+var idade = prompt('Em que ano você nasceu?')
+
+  switch (parseInt(idade)){
+
+    case 2011:
+      window.location.href = "home_default.html"
+        break
+
+    case 2005:
+      window.location.href = "homejovem.html"
+      break
+    
+      case 2004:
+        window.location.href = "home_adulto.html"
+        break
+      
+
+      default:
+        alert('nenhum comando válido')
+        break
+    
+  }
